6e05a42387c9cc20982cd7e3eeda1450c4563e63
Home-PC <davrbankit@gmail.com>
2025-04-18 22:30:43 +0500

    first commit

---
+++ src/main/kotlin/uz/nodir/common/CommonDtoApplication.kt
@@ -0,0 +1,11 @@
+package uz.nodir.common
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CommonDtoApplication
+
+fun main(args: Array<String>) {
+    runApplication<CommonDtoApplication>(*args)
+}
